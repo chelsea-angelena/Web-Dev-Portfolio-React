7a0d71f936a08887d9993c24cca5e305f88a4eae
Chelsea Angelena <chelsea.angelena@gmail.com>
2020-10-21 19:43:33 -0700

    fixed links in projects section

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -8,38 +8,45 @@ import { Link, Route } from 'react-router-dom';
 
 const content = [
 	{
+		id: 1,
+		tech: 'React-Native',
+		tech2: 'Firebase',
 		image: require('../../assets/saturday.png'),
 		title: 'Saturday Market',
-		id: 1,
 		text:
 			"		This is a mobile application built using react-native. It's a social app and virtual market place for selling handcrafted goods, art, etc. Currently it has been built for ios, and will be released for an Apple Test Flight in the next few days. The Android version will be coming soon, as well as new features such as in app messaging.",
-		link: 'https://github.com/chelsea-angelena/Expo-Saturday-Market',
-		linkName: '/B',
-		demolink: 'https://github.com/chelsea-angelena/Expo-Saturday-Market',
-		demolinkName: '/B2',
+		link: 'https://github.com/chelsea-angelena/SaturdayMarket',
+		linkName: '/Saturday-Market',
+		demolink: 'https://chelsea-angelena.github.io/',
+		demolinkName: '/saturday-demo',
 	},
 	{
 		id: 2,
+		tech: 'React-Native',
+		tech2: 'OMDB API',
 		image: require('../../assets/simulatorshot.png'),
 		title: 'OMDB Movie App',
 		text:
-			'Built with React Native. Simple Movie search app. Save your favorite movies',
-		link: 'https://github.com/chelsea-angelena/react-native-omdb-redux',
-		linkName: '/C',
-		demolink: 'https://github.com/chelsea-angelena/Expo-Saturday-Market',
-		demolinkName: '/C2',
+			'Built with React Native and the open movie database API. Search for your favorite movies and save the list to reference later',
+		link: 'https://github.com/chelsea-angelena/react-native-omdb',
+		linkName: '/omdb-react-native',
+		demolink: 'https://chelsea-angelena.github.io/',
+		demolinkName: '/omdb-demo',
 	},
 
 	{
 		id: 3,
+		tech: 'React-Native',
+		tech2: 'Yelp API',
 		image: 'https://picsum.photos/320/220',
-		title: 'Project #3  Coming Soon',
+		title: 'Good Eats',
 		text:
-			"		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'",
-		link: 'https://github.com/chelsea-angelena/',
-		linkName: '/M',
-		demolink: 'https://github.com/chelsea-angelena/Expo-Saturday-Market',
-		demolinkName: '/M2',
+			'React-Native restaurant search App. Set to automatically use location so all search results are local. Find the hours, location, ratings and price range of restuarants near you.',
+		link:
+			'https://github.com/chelsea-angelena/react-native-local-restaurant-search',
+		linkName: '/goodEats',
+		demolink: 'https://chelsea-angelena.github.io/',
+		demolinkName: '/goodEatsDemo',
 	},
 
 	{
@@ -49,22 +56,21 @@ const content = [
 		text:
 			"		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut  in culpa qui officia deserunt mollit anim id est laborum.'",
 		link: 'https://github.com/chelsea-angelena/',
-		linkName: '/E',
-		demolink: 'https://github.com/chelsea-angelena/Expo-Saturday-Market',
-		demolinkName: '/E2',
+		linkName: '/comingsoonCode2',
+		demolink: 'https://chelsea-angelena.github.io/',
+		demolinkName: '/comingsoonDemo2',
 	},
 	{
 		id: 5,
 		image:
 			'https://images.unsplash.com/photo-1599913824804-2d4c8a33a3e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-		title: 'Project #5  Coming Soon',
-		text:
-			"		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut  in culpa qui officia deserunt mollit anim id est laborum.'",
+		title: 'Project #5',
+		text: '	Coming Soon',
 
 		link: 'https://github.com/chelsea-angelena/',
-		linkName: '/T',
-		demolink: 'https://github.com/chelsea-angelena/Expo-Saturday-Market',
-		demolinkName: '/T2',
+		linkName: '/comingsoonCode',
+		demolink: 'https://chelsea-angelena.github.io/',
+		demolinkName: '/comingsoonDemo',
 	},
 ];
 
@@ -85,28 +91,24 @@ export default function Portfolio() {
 						{content.map((content) => (
 							<div key={content.id}>
 								<Card style={styles.card} className='mb-2 text-center'>
+									<Card.Header>{content.tech}</Card.Header>
 									<div>
-										<Card.Header>Featured</Card.Header>
+										<Card.Header>{content.tech2}</Card.Header>
+										<div></div>
 										<Card.Img
 											variant='top'
+											style={styles.image}
 											src={content.image}
 											alt=''
 											className='projects__card--img'
 										/>
 										<Card.Body>
-											<Card.Title>{content.title}</Card.Title>
-											<Card.Text>{content.text}</Card.Text>
+											<Card.Title style={styles.title}>
+												{content.title}
+											</Card.Title>
+											<Card.Text style={styles.text}>{content.text}</Card.Text>
 											<Card.Body></Card.Body>
 										</Card.Body>
-										<Card.Footer className='text-muted'>
-											<Route
-												path={content.linkName}
-												component={() => {
-													window.location.href = content.link;
-												}}
-											/>
-											<Link to={content.linkName}>Demo</Link>
-										</Card.Footer>
 										<Card.Footer className='text-muted'>
 											<Route
 												path={content.demolinkName}
@@ -114,8 +116,26 @@ export default function Portfolio() {
 													window.location.href = content.demolink;
 												}}
 											/>
-											<Link to={content.demolinkName}>
-												<FontAwesomeIcon size='2x' icon={['fa', 'code']} />
+											<Link
+												to={content.demolinkName}
+												style={{ color: 'black' }}
+											>
+												Demo
+											</Link>
+										</Card.Footer>
+										<Card.Footer className='text-muted'>
+											<Route
+												path={content.linkName}
+												component={() => {
+													window.location.href = content.link;
+												}}
+											/>
+											<Link to={content.linkName}>
+												<FontAwesomeIcon
+													size='2x'
+													icon={['fa', 'code']}
+													color='black'
+												/>
 											</Link>
 										</Card.Footer>
 									</div>
@@ -126,6 +146,29 @@ export default function Portfolio() {
 						))}
 					</ol>
 				</div>
+			</div>
+			<div>
+				{/* <div style={styles.row}>
+					<img
+						src={require('../../assets/logo-logomark.png')}
+						alt='logomarkFirebase'
+						width='50px'
+					/>
+					<img
+						src={require('../../assets/reactnative.png')}
+						alt='reactnative'
+						width='50px'
+					/>
+
+					<FontAwesomeIcon
+						size='3x'
+						icon={['fab', 'app-store-ios']}
+						color='black'
+					/>
+
+					<FontAwesomeIcon size='2x' icon={['fab', 'android']} color='black' /> */}
+				{/*
+					<FontAwesomeIcon size='3x' icon={['fab', 'react']} color='black' /> */}
 			</div>
 		</div>
 	);
@@ -141,5 +184,21 @@ const styles = {
 		backgroundColor: 'white',
 		maxWidth: '24rem',
 		marginLeft: 24,
+	},
+	text: {
+		fontFamily: 'Montserrat',
+		fontWeight: 200,
+	},
+	title: {
+		fontSize: 28,
+		fontWeight: 'bold',
+	},
+	image: {
+		height: 300,
+	},
+	row: {
+		display: 'flex',
+		flexDirection: 'row',
+		width: 300,
 	},
 };
